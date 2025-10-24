b51ec52ae625e465297fa6ed8095d029ea697ee6
aupontocortes-tech <aupontocortes-tech@users.noreply.github.com>
2025-10-24 02:12:36 -0300

    feat(next): adicionar Next.js (App Router + Tailwind) em next-app e configurar vercel para @vercel/next

---
+++ next-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen grid place-items-center">
+      <h1 className="text-2xl font-semibold">Aplicativo funcionando!</h1>
+    </main>
+  )
+}
