f853bea99bd2cb2cb6c08935456312c212c2e264
aupontocortes-tech <aupontocortes-tech@users.noreply.github.com>
2025-10-24 01:39:11 -0300

    chore: simplificar entrypoint (index.tsx) no formato main.jsx

--- bat-app/src/index.tsx
+++ bat-app/src/index.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-// Register minimal service worker for PWA installability
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .catch((err) => console.error('SW registration failed', err));
-  });
-}
